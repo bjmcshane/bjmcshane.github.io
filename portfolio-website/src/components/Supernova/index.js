f8610a7ff65c006396e9f0e96c976bab186152e7
bjmcshane <bmcshane@iu.edu>
2023-01-28 23:32:00 -0600

    fixed supernova page bug

--- portfolio-website/src/components/Supernova/index.js
+++ portfolio-website/src/components/Supernova/index.js
@@ -1,8 +1,27 @@
 import './index'
+import AnimatedLetters from '../AnimatedLetters'
+import React, { useState } from 'react'
 
 const Supernova = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const aboutArr = 'Supernova Detector'.split('')
+
     return (
-        <></>
+        <div className="container supernova-page">
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters letterClass={letterClass} strArray={aboutArr} idx={15} />
+                </h1>
+                <p>
+                    I've always been interested in space! Half the reason I've taken an interest in computers
+                    and technology is because I knew it may well be a viable route for me to contribute to space
+                    exploration, one way or another.
+                </p>
+                <p>
+
+                </p>
+            </div>
+        </div>
     )
 }
 
